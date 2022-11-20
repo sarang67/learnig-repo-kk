8bf5e8dfa376e221b52ba8dacb9a1f336ee7604e
sarang <sarang>
2022-11-20 15:03:05 +0530

    git status

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ function add(no1, no2) {
   return no1 + no2;
 }
 
+function minus(no1, no2) {
+  return no1 - no2;
+}
+
+function multiply(no1, no2) {
+  return no1 * no2;
+}
+
 console.log(add(10, 20));
