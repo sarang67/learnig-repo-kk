d51af669fc1bfdb563ce4012302e970cfb751054
sarang <sarang>
2022-11-20 15:15:17 +0530

    remove all the code

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-console.log("This is my first console");
 
-function add(no1, no2) {
-  return no1 + no2;
-}
-
-function minus(no1, no2) {
-  return no1 - no2;
-}
-
-function multiply(no1, no2) {
-  return no1 * no2;
-}
-
-console.log(add(10, 20));
